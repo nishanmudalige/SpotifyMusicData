9e8070a522985aa731d9eb0acaee24e2046777fe
Nishan Mudalige <30182885+nishanmudalige@users.noreply.github.com>
2025-03-02 16:29:17 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+from dotenv import load_dotenv
+import os
+import base64
+from requests import post, get
+import json
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+# print(client_id, cli)
+
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode('utf-8')
+    auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+def get_auth_header(token):
+    return{"Authorization": "Bearer " + token}
+
+
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={artist_name}&type=artist&limit=1"
+    
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["artists"]["items"]
+    if len(json_result) == 0:
+        print("No artist with this name exists")
+        return None
+    
+    # print(json_result)
+    return(json_result[0])
+
+
+def get_top_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?market=US"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
+
+def get_all_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/tracks?ids={artist_id}"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)
+    return json_result
+
+
+token = get_token()
+# print(token)
+result = search_for_artist(token, "Michael Jackson")
+# print(result)
+# print(result["name"])
+
+artist_id = result["id"]
+top_songs = get_top_songs_by_artist(token, artist_id)
+print(top_songs)
+
+# artist_id = result["id"]
+# all_songs = get_all_songs_by_artist(token, artist_id)
+# print(all_songs)
+
+for idx, song in enumerate(top_songs):
+    print(f"{idx + 1}. {song['name']}")
+
+
+# for idx, song in enumerate(all_songs):
+#     print(f"{idx + 1}. {song['name']}")
